e2778016a66d225fa6858ec48eefb9c7294788cc
keyboardturner <20036792+keyboardturner@users.noreply.github.com>
2024-12-09 08:32:23 -0800

    de-english-ify

--- HeckTradeChat.lua
+++ HeckTradeChat.lua
@@ -1,5 +1,12 @@
 local function heck()
-	LeaveChannelByName("Trade")
+	for i = 1, GetNumDisplayChannels() do
+		local channel = C_ChatInfo.GetChannelInfoFromIdentifier(i);
+		if channel and channel.shortcut and channel.zoneChannelID then
+			if channel.zoneChannelID == 2 or channel.zoneChannelID == 42 then -- Trade / Services
+				LeaveChannelByName(channel.shortcut)
+			end
+		end
+	end
 end
 local darn = CreateFrame("Frame")
 darn:RegisterEvent("CHANNEL_UI_UPDATE")
